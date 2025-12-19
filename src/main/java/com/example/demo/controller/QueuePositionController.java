9d09a224df56b725e0713db90cfe80a85ad8f684
bala-amypo <bala@amypo.in>
2025-12-19 10:15:13 +0000

    User commit

--- src/main/java/com/example/demo/controller/QueuePositionController.java
+++ src/main/java/com/example/demo/controller/QueuePositionController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class QueuePositionController {
+public class QueuePositionController.java {
 
     @GetMapping("/queue")
     public String queue() {
