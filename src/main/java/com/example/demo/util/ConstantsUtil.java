8e0e103bfb267f02f5033ff6623a572f47a8810a
bala-amypo <bala@amypo.in>
2025-12-25 17:59:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/util/ConstantsUtil.java
@@ -0,0 +1,12 @@
+package com.example.demo.util;
+
+public class ConstantsUtil {
+
+    public static final String STATUS_WAITING = "WAITING";
+    public static final String STATUS_SERVING = "SERVING";
+    public static final String STATUS_COMPLETED = "COMPLETED";
+    public static final String STATUS_CANCELLED = "CANCELLED";
+}
+
+
+
