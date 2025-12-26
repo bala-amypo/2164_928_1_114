e901f140e0644ad00f385eaa060e75bcb7c0d1ac
bala-amypo <bala@amypo.in>
2025-12-26 06:28:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/Token.java
+++ src/main/java/com/example/demo/entity/Token.java
@@ -4,50 +4,16 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "tokens")
 public class Token {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // WAITING, SERVING, COMPLETED, CANCELLED
-    @Column(nullable = false)
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private TokenStatus status;
 
     private LocalDateTime completedAt;
 
-    @Column(nullable = false)
-    private int queuePosition;
-
-    public Token() {}
-
     // getters and setters
-    public Long getId() {
-        return id;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getCompletedAt() {
-        return completedAt;
-    }
-
-    public void setCompletedAt(LocalDateTime completedAt) {
-        this.completedAt = completedAt;
-    }
-
-    public int getQueuePosition() {
-        return queuePosition;
-    }
-
-    public void setQueuePosition(int queuePosition) {
-        this.queuePosition = queuePosition;
-    }
 }
