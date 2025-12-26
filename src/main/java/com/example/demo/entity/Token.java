eee9747efbf60f88a8a927988edbc625d61a3d72
bala-amypo <bala@amypo.in>
2025-12-26 06:11:07 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Token.java
@@ -0,0 +1,53 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tokens")
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // WAITING, SERVING, COMPLETED, CANCELLED
+    @Column(nullable = false)
+    private String status;
+
+    private LocalDateTime completedAt;
+
+    @Column(nullable = false)
+    private int queuePosition;
+
+    public Token() {}
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    public int getQueuePosition() {
+        return queuePosition;
+    }
+
+    public void setQueuePosition(int queuePosition) {
+        this.queuePosition = queuePosition;
+    }
+}
