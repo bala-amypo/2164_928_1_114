02ff0266dd53814db3e649999f75d5b58cca82a8
bala-amypo <bala@amypo.in>
2025-12-27 04:53:49 +0000

    User commit

--- src/main/java/com/example/demo/repository/ServiceCounterRepository.java
+++ src/main/java/com/example/demo/repository/ServiceCounterRepository.java
@@ -1,4 +1,4 @@
-vpackage com.example.demo.repository;
+package com.example.demo.repository;
 
 import com.example.demo.entity.ServiceCounter;
 import org.springframework.data.jpa.repository.JpaRepository;
