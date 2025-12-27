767f57c2084b053f1f2bbe48f3cba2a08980b98d
bala-amypo <bala@amypo.in>
2025-12-27 09:28:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/QueuePosition.java
+++ src/main/java/com/example/demo/entity/QueuePosition.java
@@ -1,22 +1,46 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Table(name = "queue_positions")
 public class QueuePosition {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    private Token token;
+    @Column(name = "token_id")
+    private Long tokenId;
 
+    @Column(name = "position")
     private Integer position;
+
+    // -------- Getters --------
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getTokenId() {
+        return tokenId;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    // -------- Setters --------
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTokenId(Long tokenId) {
+        this.tokenId = tokenId;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
 }
