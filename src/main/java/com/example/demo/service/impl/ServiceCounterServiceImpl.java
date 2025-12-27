aa4f6efe5150de219e8029f2e1f1f0c97f11fc47
bala-amypo <bala@amypo.in>
2025-12-27 04:23:24 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ServiceCounterServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ServiceCounterServiceImpl.java
@@ -3,24 +3,26 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.ServiceCounter;
 import com.example.demo.repository.ServiceCounterRepository;
 import com.example.demo.service.ServiceCounterService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service   // 🔴 REQUIRED to create Spring bean
 public class ServiceCounterServiceImpl implements ServiceCounterService {
 
-    private final ServiceCounterRepository repository;
+    private final ServiceCounterRepository counterRepository;
 
-    public ServiceCounterServiceImpl(ServiceCounterRepository repository) {
-        this.repository = repository;
+    public ServiceCounterServiceImpl(ServiceCounterRepository counterRepository) {
+        this.counterRepository = counterRepository;
     }
 
     @Override
     public ServiceCounter addCounter(ServiceCounter counter) {
-        return repository.save(counter);
+        return counterRepository.save(counter);
     }
 
     @Override
     public List<ServiceCounter> getActiveCounters() {
-        return repository.findByIsActiveTrue();
+        return counterRepository.findByActiveTrue();
     }
 }
